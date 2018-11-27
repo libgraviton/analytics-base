078f1a49b558f7d0da6d909be7f560610bcb5ea7
narcoticfresh <dn@xbe.ch>
2018-11-27 15:02:38 +0100

    allow to check multiple variables and negate

--- src/Pipeline/PipelineAbstract.php
+++ src/Pipeline/PipelineAbstract.php
@@ -11,52 +11,76 @@ namespace Graviton\AnalyticsBase\Pipeline;
  */
 abstract class PipelineAbstract {
 
-	protected $params = [];
-	protected const EMPTY_STRING = '__EMPTY__';
-	protected $cleanCount = 4;
+    protected $params = [];
+    protected const EMPTY_STRING = '__EMPTY__';
+    protected $cleanCount = 4;
 
-	public function setParams(array $params) {
-		$this->params = $params;
-	}
+    public function setParams(array $params) {
+        $this->params = $params;
+    }
 
-	public function hasParam($paramName) {
-		return isset($this->params[$paramName]);
-	}
+    public function hasParam($paramName, $checkMode = 'and') {
+        if (is_null($checkMode)) {
+            $checkMode = 'and';
+        }
 
-	public function getParam($paramName) {
-		if ($this->hasParam($paramName)) {
-			return $this->params[$paramName];
-		}
-		return null;
-	}
+        if (!is_array($paramName)) {
+            $paramName = [$paramName];
+        }
 
-	public function get() {
-		$cleaned = $this->cleanElements($this->getPipeline());
-		for ($i = 0; $i < ($this->cleanCount+1); $i++) {
-			$cleaned = $this->cleanElements($cleaned);
-		}
-		return array_values($cleaned);
-	}
+        foreach ($paramName as $singleParamName) {
+            $exists = isset($this->params[$singleParamName]);
 
-	private function cleanElements($pipeline) {
-		$cleaned = [];
-		foreach ($pipeline as $key => $value) {
-			if (is_array($value)) {
-				$value = $this->cleanElements($value);
-			}
+            if ($exists === true && $checkMode == 'or') {
+                return true;
+            }
 
-			if ((!is_array($value) && $value !== self::EMPTY_STRING) || (is_array($value) && !empty($value))) {
-				if (!is_numeric($key)) {
-					$cleaned[$key] = $value;
-				} else {
-					$cleaned[] = $value;
-				}
-			}
-		}
+            if ($exists === false && $checkMode == 'and') {
+                return false;
+            }
+        }
 
-		return $cleaned;
-	}
+        if ($checkMode == 'or') {
+            return false;
+        }
 
-	abstract protected function getPipeline();
+        return true;
+    }
+
+    public function getParam($paramName) {
+        if ($this->hasParam($paramName)) {
+            return $this->params[$paramName];
+        }
+        return null;
+    }
+
+    public function get() {
+        $cleaned = $this->cleanElements($this->getPipeline());
+        for ($i = 0; $i < ($this->cleanCount+1); $i++) {
+            $cleaned = $this->cleanElements($cleaned);
+        }
+        return array_values($cleaned);
+    }
+
+    private function cleanElements($pipeline) {
+        $cleaned = [];
+        foreach ($pipeline as $key => $value) {
+            if (is_array($value)) {
+                $value = $this->cleanElements($value);
+            }
+
+            if ((!is_array($value) && $value !== self::EMPTY_STRING) || (is_array($value) && !empty($value))) {
+                if (!is_numeric($key)) {
+                    $cleaned[$key] = $value;
+                } else {
+                    $cleaned[] = $value;
+                }
+            }
+        }
+
+        return $cleaned;
+    }
+
+    abstract protected function getPipeline();
 
 }
